506eb26d62772fe18d63d70c76db66bdd360ff60
kyziak <asibarrao@unal.edu.co>
2021-09-10 10:13:14 -0500

    implemented drawing of a figure

--- canva/dibujo.js
+++ canva/dibujo.js
@@ -1,8 +1,45 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var lineas = 30;
+var color_borde = "green";
 
-drawLines("pink", 10,300,200,10);
-drawLines("yellow", 300,10,10,220);
+// bottom-left corner
+for (let l = 0; l < lineas; l++) {
+    drawLines("#FAA", 0, 10*l, 10*(l + 1), 300);  
+}
+
+//top-right corner 
+for (let l = 0; l < lineas; l++) {
+    drawLines("#FAA", 10*l, 0, 300, 10*(l + 1));  
+}
+
+// center first cuadrant
+for (let i = 0; i < lineas/2; i++) {
+    drawLines("#AAF", 150, 10*i, 150 + 10*(i + 1), 150);  
+}
+
+//center second cuadrant
+for (let i = 0; i < lineas/2; i++) {
+    drawLines("#AAF", 10*i, 150, 150, 150 - 10*(i + 1));  
+}
+
+//center third cuadrant
+for (let i = 0; i < lineas/2; i++) {
+    drawLines("#AAF", 10*i, 150, 150, 150 + 10*(i + 1)); 
+}
+
+//center fourth cuadrant
+for (let i = 0; i < lineas/2; i++) {
+    drawLines("#AAF", 150, 300 - 10*i, 150 + 10*(i + 1), 150); 
+}
+
+//central and border lines
+drawLines(color_borde, 1, 1, 1, 300); 
+drawLines(color_borde, 1, 299, 299, 299); 
+drawLines(color_borde, 299, 1, 299, 299); 
+drawLines(color_borde, 1, 1, 299, 1); 
+drawLines("#AAF", 0, 150, 300, 150); 
+drawLines("AAF", 150, 1, 150, 299); 
 
 function drawLines(color, xinit, yinit, xend, yend){
     lienzo.beginPath();
@@ -12,4 +49,3 @@ function drawLines(color, xinit, yinit, xend, yend){
     lienzo.stroke();
     lienzo.closePath();
 }
-
