c22fb4fde1487d3df9de105dfa4f757f99ea7e47
kyziak <asibarrao@unal.edu.co>
2021-09-13 10:00:18 -0500

    Implemented formularies that ask for line number

--- canva/dibujo.js
+++ canva/dibujo.js
@@ -1,46 +1,13 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var color_borde = "green";
+var ancho = d.width;
 
-// bottom-left corner
-for (let l = 0; l < lineas; l++) {
-    drawLines("#FAA", 0, 10*l, 10*(l + 1), 300);  
-}
+// traer botón y caja de texto
+var txt = document.getElementById("text_lines");
+var button = document.getElementById("button_lines");
+button.addEventListener("click", drawByClick);
 
-//top-right corner 
-for (let l = 0; l < lineas; l++) {
-    drawLines("#FAA", 10*l, 0, 300, 10*(l + 1));  
-}
-
-// center first cuadrant
-for (let i = 0; i < lineas/2; i++) {
-    drawLines("#AAF", 150, 10*i, 150 + 10*(i + 1), 150);  
-}
-
-//center second cuadrant
-for (let i = 0; i < lineas/2; i++) {
-    drawLines("#AAF", 10*i, 150, 150, 150 - 10*(i + 1));  
-}
-
-//center third cuadrant
-for (let i = 0; i < lineas/2; i++) {
-    drawLines("#AAF", 10*i, 150, 150, 150 + 10*(i + 1)); 
-}
-
-//center fourth cuadrant
-for (let i = 0; i < lineas/2; i++) {
-    drawLines("#AAF", 150, 300 - 10*i, 150 + 10*(i + 1), 150); 
-}
-
-//central and border lines
-drawLines(color_borde, 1, 1, 1, 300); 
-drawLines(color_borde, 1, 299, 299, 299); 
-drawLines(color_borde, 299, 1, 299, 299); 
-drawLines(color_borde, 1, 1, 299, 1); 
-drawLines("#AAF", 0, 150, 300, 150); 
-drawLines("AAF", 150, 1, 150, 299); 
-
+//Función para dibujar lineas
 function drawLines(color, xinit, yinit, xend, yend){
     lienzo.beginPath();
     lienzo.strokeStyle = color;
@@ -48,4 +15,50 @@ function drawLines(color, xinit, yinit, xend, yend){
     lienzo.lineTo(xend, yend);
     lienzo.stroke();
     lienzo.closePath();
+}
+
+//Función para dibujar por click
+function drawByClick(){
+    var lineas = parseInt(txt.value);    
+    var color_borde = "green";
+    var espacio = ancho/lineas;
+    lienzo.clearRect(0, 0, d.width, d.height);
+
+    // bottom-left corner
+    for (let l = 0; l < lineas; l++) {
+            drawLines("#FAA", 0, espacio*l, espacio*(l + 1), 300);  
+        }
+
+    //top-right corner 
+    for (let l = 0; l < lineas; l++) {
+        drawLines("#FAA", espacio*l, 0, 300, espacio*(l + 1));  
+    }
+
+    // center first cuadrant
+    for (let i = 0; i < lineas/2; i++) {
+        drawLines("#AAF", 150, espacio*i, 150 + espacio*(i + 1), 150);  
+    }
+
+    //center second cuadrant
+    for (let i = 0; i < lineas/2; i++) {
+        drawLines("#AAF", espacio*i, 150, 150, 150 - espacio*(i + 1));  
+    }
+
+    //center third cuadrant
+    for (let i = 0; i < lineas/2; i++) {
+        drawLines("#AAF", espacio*i, 150, 150, 150 + espacio*(i + 1)); 
+    }
+
+    //center fourth cuadrant
+    for (let i = 0; i < lineas/2; i++) {
+        drawLines("#AAF", 150, 300 - espacio*i, 150 + espacio*(i + 1), 150); 
+    }
+
+    //central and border lines
+    drawLines(color_borde, 1, 1, 1, 300); 
+    drawLines(color_borde, 1, 299, 299, 299); 
+    drawLines(color_borde, 299, 1, 299, 299); 
+    drawLines(color_borde, 1, 1, 299, 1); 
+    drawLines("#AAF", 0, 150, 300, 150); 
+    drawLines("AAF", 150, 1, 150, 299); 
 }
