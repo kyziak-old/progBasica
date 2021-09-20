b504a24de88771d248ec787692cb94c5ac923e3e
kyziak <asibarrao@unal.edu.co>
2021-09-20 17:34:46 -0500

    Projecto de cajero

--- cajero/src/billetes.js
+++ cajero/src/billetes.js
@@ -2,5 +2,7 @@ class Billete{
     constructor(valor, cantidad){
         this.valor = valor;
         this.cantidad = cantidad;
+        this.img = new Image();
+        this.img.src = imagenVector[this.valor];
     }
 }
--- cajero/src/cajero.js
+++ cajero/src/cajero.js
@@ -1,11 +1,20 @@
 var caja = [];
 var entregado = [];
 
-caja.push(new Billete(100, 5));
+var imagenVector = [];
+//image["100"] = "img/100bill.png";
+imagenVector["50"] = "img/billete50.png";
+imagenVector["20"] = "img/billete20.png";
+imagenVector["10"] = "img/billete10.png";
+//image["5"] = "img/5bill.jpg";
+
+//caja.push(new Billete("cien", 100, 5));
 caja.push(new Billete(50, 10));
 caja.push(new Billete(20, 30));
 caja.push(new Billete(10, 30));
-caja.push(new Billete(5, 5));
+//caja.push(new Billete("cinco", 5, 5));
+
+console.log(caja);
 
 var dinero = 0;
 var div = 0;
@@ -36,8 +45,13 @@ function takeMoney(){
     }else{
         for(var e of entregado){
             if(e.cantidad > 0){
-                resultado.innerHTML += " billetes de $" + e.valor + "<br />";
+                resultado.innerHTML += e.cantidad + " billetes de $" + e.valor + "<br />";
             }
         }
+        discountMoney();
     }
+}
+
+function discountMoney(){
+    console.log(imagenVector);
 }
